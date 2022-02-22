13fc9867cbc27787b306023d4c0493cc5466f59f
pi:no <60024401+konamilk@users.noreply.github.com>
2022-02-22 16:05:15 +0900

    Update spec_augment_pytorch.py

--- SpecAugment/spec_augment_pytorch.py
+++ SpecAugment/spec_augment_pytorch.py
@@ -39,7 +39,7 @@ import librosa.display
 import numpy as np
 import random
 import matplotlib
-matplotlib.use('TkAgg')
+# matplotlib.use('TkAgg')
 import matplotlib.pyplot as plt
 from SpecAugment.sparse_image_warp_pytorch import sparse_image_warp
 import torch
